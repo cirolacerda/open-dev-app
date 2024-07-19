c28c87be1ba165994dc566d7ba29413af3f7ec2d
Ciro Lacerda <cirolacerda@gmail.com>
2024-07-18 23:48:58 -0300

    Ajustando para enum

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\RoleEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,19 +17,19 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@teste.com',
-            'role_id' => 1
+            'role_id' => RoleEnum::ADMIN
         ]);
 
         User::factory()->create([
             'name' => 'CTO',
             'email' => 'cto@teste.com',
-            'role_id' => 2
+            'role_id' => RoleEnum::CTO
         ]);
 
         User::factory()->create([
             'name' => 'Assistant',
             'email' => 'assistant@teste.com',
-            'role_id' => 3
+            'role_id' => RoleEnum::ASSISTANT
         ]);
     }
 }
