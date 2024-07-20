038097dc8f857a2efc296d48970e6d2f4892f2bc
Ciro Lacerda <cirolacerda@gmail.com>
2024-07-19 23:22:27 -0300

    Criando gates before, search devs e manage assistants

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\RoleEnum;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Define gate before dando acesso ao admin
+        Gate::before(function($user){
+            if($user->role_id == RoleEnum::ADMIN){
+                return true;
+            }
+        });
+
+        //Define gate para acesso a busca do dev e listagem
+        Gate::define('search-devs', function($user) {
+            return $user->role_id == RoleEnum::CTO ||
+                   $user->role_id == RoleEnum::ASSISTANT;
+        });
+
+        //Define gate para acesso ao gerenciamento e cadastro de assistentes
+        Gate::define('manage-assistants', function ($user){
+            return $user->role_id == RoleEnum::CTO;
+        });
     }
 }
