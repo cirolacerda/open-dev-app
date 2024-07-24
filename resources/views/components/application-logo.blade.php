22fd25880a4cbca7724e4ea7669a23e79f9af813
Ciro Lacerda <cirolacerda@gmail.com>
2024-07-23 23:52:39 -0300

    Ajuste logo e conversão tailwind

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,36 +1,9 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 90" width="50" height="50" {{ $attributes }}>
-    <style>
-        .background {
-            fill: #000000;
-            /* cor de fundo preta */
-        }
-
-        .open {
-            fill: #ffffff;
-            /* cor do texto "open" branca */
-            font-family: Arial;
-            font-size: 21px;
-        }
-
-        .dev {
-            fill: #ffffff;
-            /* cor do texto "dev" branca */
-            font-family: Arial;
-            font-size: 14px;
-        }
-
-        .icon {
-            fill: none;
-            /* sem preenchimento */
-            stroke: #ffffff;
-            /* cor do traço branca */
-            stroke-width: 2;
-            /* largura do traço */
-        }
-    </style>
-    <circle class="background" cx="50" cy="50" r="45" />
-    <text class="open" x="47" y="43" text-anchor="middle">open</text>
-    <text class="dev" x="55" y="67" text-anchor="middle">&#60;dev&#62; </dev></text>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="66" height="65" {{ $attributes }}>
+    <circle class="fill-black dark:fill-gray-500" cx="50" cy="50" r="45" />
+    <text class="fill-white dark:fill-black font-sans text-[21px]" x="47" y="43" text-anchor="middle">open</text>
+    <text class="fill-white dark:fill-black font-sans text-[14px]" x="55" y="67" text-anchor="middle">&lt;dev&gt;
+    </text>
     <!-- Ícone de computador e programador -->
-    <rect class="icon" x="20" y="20" width="60" height="60" rx="2" ry="2" />
+    <rect class="fill-transparent stroke-white dark:stroke-black stroke-2" x="20" y="20" width="60" height="60"
+        rx="2" ry="2"></rect>
 </svg>
