5e0c7f48cb9052484e648a943ef1df09dea323cd
Ciro Lacerda <cirolacerda@gmail.com>
2024-07-23 23:52:01 -0300

    Ajustes modo dark

--- resources/views/assistant/create.blade.php
+++ resources/views/assistant/create.blade.php
@@ -9,7 +9,7 @@
 
         <div class="py-2">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
                     <form method="POST" action="{{ route('assistants.store') }}">
                         @csrf
 
