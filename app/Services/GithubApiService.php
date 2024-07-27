39d6b8015fa9de2022a5569a464e34f45cb5ba91
Ciro Lacerda <cirolacerda@gmail.com>
2024-07-27 00:28:20 -0300

    Criando paginação padrão laravel

--- app/Services/GithubApiService.php
+++ app/Services/GithubApiService.php
@@ -2,19 +2,25 @@
 
 namespace App\Services;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Http;
 
 class GithubApiService {
     private $baseUrl = 'https://api.github.com';
 
-    public function searchDevs($query) {
+    public function searchDevs($query, $page = 1, $perPage = 10) {
         $response = Http::withHeaders([
             'Authorization' => 'token ' . env('GITHUB_TOKEN'),
         ])->get("$this->baseUrl/search/users", [
             'q' => $query,
-            'per_page' => 10,
+            'per_page' => $perPage,
+            'page' => $page,
+
         ]);
 
-        return $response->json();
+        $items = $response->json()['items'];
+        $total = $response->json()['total_count'];
+
+        return new LengthAwarePaginator($items,$total,$perPage,$page, [ 'path' => LengthAwarePaginator::resolveCurrentPath(),]);
     }
 }
